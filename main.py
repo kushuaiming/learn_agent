08ba1ffcbfa82ed31f4400555e3b2f41fd9c920b
xum10 <xum10@xiaopeng.com>
2026-08-20 18:59:30 +0800

    optimize style

--- main.py
+++ main.py
@@ -3,23 +3,19 @@ from core.llm import HelloAgentsLLM
 from dotenv import load_dotenv
 from tools.builtin.calculator import CalculatorTool
 
-# 加载环境变量
 load_dotenv()
 
-# 创建LLM实例 - 框架自动检测provider
 llm = HelloAgentsLLM()
 
 # 或手动指定provider（可选）
 # llm = HelloAgentsLLM(provider="modelscope")
 
-# 创建SimpleAgent
 agent = SimpleAgent(
     name="AI助手",
     llm=llm,
     system_prompt="你是一个有用的AI助手"
 )
 
-# 基础对话
 response = agent.run("你好！请介绍一下自己")
 print(response)
 
@@ -28,9 +24,7 @@ calculator = CalculatorTool()
 # 需要实现7.4.1的MySimpleAgent进行调用，后续章节会支持此类调用方式
 # agent.add_tool(calculator)
 
-# 现在可以使用工具了
-response = agent.run("请帮我计算 2 + 3 * 4")
+response = agent.run("Calculate 2 + 3 * 4")
 print(response)
 
-# 查看对话历史
-print(f"历史消息数: {len(agent.get_history())}")
+print(f"History Length: {len(agent.get_history())}")
